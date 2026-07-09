fc89fa4accf5b9a9d7cfd27e56d138a2e5ac56a0
thuhuong <buithithuhuong4494@gmail.com>
2026-07-09 15:19:10 +0700

    doi nhanhh

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,6 +1,6 @@
-import ShopBanner from "@/components/shop/ShopBanner";
 import ShopFilterBar from "@/components/shop/ShopFilterBar";
 
+import ShopBanner from "@/components/common/ShopBanner";
 import ProductGrid from "@/components/common/ProductGrid";
 import Pagination from "@/components/common/Pagination";
 import FeatureSection from "@/components/common/FeatureSection";
