62b067a8e9549cf71b011ece27f18dab05ea4cbe
thuhuong <buithithuhuong4494@gmail.com>
2026-07-13 22:25:04 +0700

    Da merge voi testHP tren TH

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,7 +1,9 @@
 import ProductGrid from "@/components/common/ProductGrid";
 import Pagination from "@/components/shop/Pagination";
 import FeatureSection from "@/components/common/FeatureSection";
+import ShopBanner from "@/components/common/ShopBanner";
 import products from "@/data/products.json";
+
 
 export const metadata = {
   title: "Shop",
@@ -12,7 +14,6 @@ export default function ShopPage() {
   return (
     <>
       <ShopBanner title="Shop" current="Shop" />
-      <ShopFilterBar />
       <ProductGrid title="" products={products} showMore={false} />
       <Pagination />
       <FeatureSection />
